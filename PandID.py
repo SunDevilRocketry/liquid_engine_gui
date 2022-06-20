855ebea382058f28375982a9d84e96c16b72cd06
Colton Acosta <cacost12@asu.edu>
2022-06-19 20:55:17 -0700

    rename self.c object in PandID header

--- PandID.py
+++ PandID.py
@@ -22,18 +22,31 @@ import tkinter as tk
 import DiagramComponents
 import main
 
+
+###############################################################
+# Engine Title Object                                         #
+###############################################################
 class Header:
 
-    def __init__(self, root, background, text, width, height, size):
-        self.c = tk.Canvas(root, width=width, height=height, bg=background, highlightthickness=0)
+    def __init__(
+                self, 
+                root,       # window to draw on 
+                background, # background color 
+                text,       # header text 
+                width,      
+                height, 
+                size
+                ):
+
+        self.canvas = tk.Canvas(root, width=width, height=height, bg=background, highlightthickness=0)
         self.width = width
         self.height = height
 
-        self.c.create_line((width / 8.0, 7 * height / 8.0), (7 * width / 8.0, 7 * height / 8.0), width=1, fill='white')
-        self.c.create_text(width / 2.0, height / 2.0, font=("Arial", size, ''), fill="white", text=text)
+        self.canvas.create_line((width / 8.0, 7 * height / 8.0), (7 * width / 8.0, 7 * height / 8.0), width=1, fill='white')
+        self.canvas.create_text(width / 2.0, height / 2.0, font=("Arial", size, ''), fill="white", text=text)
 
     def getWidget(self):
-        return self.c
+        return self.canvas
 
 class Text:
 
