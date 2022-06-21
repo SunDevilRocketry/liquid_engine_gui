122de1027474ddaa25a9f7fde6774bd1b9aaaa1e
Colton Acosta <cacost12@asu.edu>
2022-06-20 18:00:28 -0700

    renamed xy to pipewall_top/bottom

--- DiagramComponents.py
+++ DiagramComponents.py
@@ -89,10 +89,10 @@ class Component:
             pipewall_right = [(9 * width / 16.0, height * (3 / 4.0)), (9 * width / 16.0, height)]
             self.canvas.create_line(pipewall_right, width=1, fill='white')
         if (pipe_left):
-            xy = [(width / 4.0, 7 * height / 16.0), (0, 7 * height / 16.0)]
-            self.canvas.create_line(xy, width=1, fill='white')
-            xy2 = [(width / 4.0, 9 * height / 16.0), (0, 9 * height / 16.0)]
-            self.canvas.create_line(xy2, width=1, fill='white')
+            pipewall_top = [(width / 4.0, 7 * height / 16.0), (0, 7 * height / 16.0)]
+            self.canvas.create_line(pipewall_top, width=1, fill='white')
+            pipewall_bottom = [(width / 4.0, 9 * height / 16.0), (0, 9 * height / 16.0)]
+            self.canvas.create_line(pipewall_bottom, width=1, fill='white')
 
         # DRAW FLOW
         self.f1 = self.canvas.create_rectangle((7 * width / 16.0) + 1, 0, (9 * width / 16.0) - 1, (height / 4.0) - 1,
