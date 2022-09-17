bd6849a2b1eff8c95d7adc35700ef2222518b7ec
Colton Acosta <cacost12@asu.edu>
2022-09-16 20:52:39 -0700

    resolved bugs in pt demo code

--- main.py
+++ main.py
@@ -62,6 +62,132 @@ def close_window_callback():
 
 
 ###############################################################
+#                                                             #
+# PROCEDURE:                                                  #
+# 		adc_readout_to_voltage                                #
+#                                                             #
+# DESCRIPTION:                                                #
+# 		displays a general software failure error message     #
+#                                                             #
+###############################################################
+def adc_readout_to_voltage( 
+                          readout, # ADC readout
+                          num_bits # number of bits in ADC readout
+                          ):
+	voltage_step = 3.3/float(2**(num_bits))
+	return readout*voltage_step 
+
+
+###############################################################
+#                                                             #
+# OBJECT:                                                     #
+# 		serialData                                            #
+#                                                             #
+# DESCRIPTION:                                                #
+# 		serial port user API and handler for passing data     #
+#       between command functions                             #
+#                                                             #
+###############################################################
+class serialData:
+    def __init__(self):
+        self.baudrate            = None
+        self.comport             = None
+        self.timeout             = None
+        self.serialObj           = serial.Serial()
+        self.config_status       = False 
+        self.controller          = None
+        self.flash_write_enabled = False 
+
+    # Initialize Serial Port
+    def initComport(self, baudrate, comport, timeout):
+        self.baudrate = baudrate 
+        self.comport = comport 
+        self.timeout = timeout 
+        self.serialObj.baudrate = self.baudrate
+        self.serialObj.port = self.comport
+        self.serialObj.timeout = self.timeout
+        self.config_status = True
+
+    # Configure Serial port from class attributes
+    def configComport(self):
+        self.serialObj.baudrate = self.baudrate
+        self.serialObj.port = self.comport
+        self.serialObj.timeout = self.timeout
+
+    # Open the serial port
+    # returns a boolean variable indicating whether the port
+    # connection was sucessful
+    def openComport(self):
+
+        # Ensure serial port has been properly configured 
+        if(not self.config_status):
+            print("Error: Cannot open serial port. Serial " +
+                  "port has not been properly configured")
+            return False
+
+        # open port
+        self.serialObj.open()
+        return True
+
+    # Close the serial port
+    # Returns a boolean value indicating whether the port connection was 
+    # successfully closed
+    def closeComport(self):
+        # check that the serial port is open
+        if (not self.serialObj.is_open):
+            print("No open serial port detected")
+            return False
+        else:
+            self.serialObj.close()
+            return True
+
+	# Check if serial port is active
+    def is_active(self):
+        return self.serialObj.is_open
+
+	# List available serial port connections
+    def list_ports(self):
+	    available_ports = serial.tools.list_ports.comports()
+	    available_port_names = []
+	    for port in available_ports:
+		    available_port_names.append(port.device)
+	    return available_port_names
+
+    # Write a single Byte to the serial port
+    def sendByte(self, byte):
+        if (not self.serialObj.is_open):
+            print("Error: Could not transmit byte over serial port. No active" \
+                   +"serial port connection")
+        else:
+            self.serialObj.write(byte)
+
+    # Write an array of bytes to the serial port 
+    def sendBytes(self, byte_array):
+        if (not self.serialObj.is_open):
+            print("Error: Could not transmit byte over serial port. No active" \
+                   +"serial port connection")
+        else:
+            for byte in byte_array:
+                self.serialObj.write(byte)
+
+    # Read a single Byte from the serial port
+    def readByte(self):
+        if (not self.serialObj.is_open):
+            print("Error: Could not read byte from serial port. No active" \
+                   +"serial port connection")
+        else:
+             return self.serialObj.read()
+
+	# Set the SDR controller to enable board-specific commands
+    def set_SDR_controller(self, controller_name):
+        self.controller = controller_name
+
+	# Reset the SDR controller to disable board-specific commands
+    def reset_SDR_controller(self):
+        self.controller = None
+
+
+###############################################################
 # Main application entry point                                #
 ###############################################################
 if __name__ == '__main__':
@@ -75,6 +201,11 @@ if __name__ == '__main__':
 
     # Initializations
     exitFlag = False
+
+	# Serial Port Object
+    serObj = serialData()
+    serObj.initComport(9600, '/dev/ttyUSB0', 1)
+    serObj.openComport()
 
 
 	###########################################################
@@ -264,49 +395,49 @@ if __name__ == '__main__':
     gauge1 =      SDR_gauge.gauge(
                                  gauge_frame_row1, 
                                  background = 'black',
-                                 max_sensor_val = 5
+                                 max_sensor_val = 1000
                                  )
 
     gauge2 =      SDR_gauge.gauge(
                                  gauge_frame_row1, 
                                  background = 'black',
-                                 max_sensor_val =  5
+                                 max_sensor_val =  1000
                                  )
 
     gauge3 =      SDR_gauge.gauge(
                                  gauge_frame_row1, 
                                  background = 'black',
-                                 max_sensor_val =  5
+                                 max_sensor_val =  1000
                                  )
 
     gauge4 =      SDR_gauge.gauge(
                                  gauge_frame_row1, 
                                  background = 'black', 
-                                 max_sensor_val = 5
+                                 max_sensor_val = 1000
                                  )
 
     gauge5 =      SDR_gauge.gauge(
                                  gauge_frame_row2, 
                                  background = 'black', 
-                                 max_sensor_val = 5
+                                 max_sensor_val = 1000 
                                  )
 
     gauge6 =      SDR_gauge.gauge(
                                  gauge_frame_row2, 
                                  background = 'black', 
-                                 max_sensor_val = 5
+                                 max_sensor_val = 1000 
                                  )
 
     gauge7 =      SDR_gauge.gauge(
                                  gauge_frame_row2, 
                                  background = 'black', 
-                                 max_sensor_val = 5
+                                 max_sensor_val = 1000
                                  )
 
     gauge8 =      SDR_gauge.gauge(
                                  gauge_frame_row2, 
                                  background = 'black', 
-                                 max_sensor_val = 5
+                                 max_sensor_val = 1000
                                  )
 
     gauge1.setText("Nan", "Fuel Tank Pressure"     )
@@ -362,15 +493,44 @@ if __name__ == '__main__':
     prevCon = True
     while (not exitFlag):
         try:
+			# Send request for reading
+            serObj.sendByte(b'\x34')
+
+			# Get Readings
+            pt_readout_bytes = []
+            for i in range( 4 ):
+                pt_readout_bytes.append( serObj.readByte() )
+
+            # Convert from bytes to voltage
+            sensor_val = 0
+            for j in range(4): # Hex to integer conversion
+                sensor_val += ( int.from_bytes( pt_readout_bytes[j], 'big') << 8*(3-j) )
+            sensor_val = adc_readout_to_voltage( sensor_val, 16 )
+
+			# Convert from voltage to psi
+            psi_readout = sensor_val*(1000.0/3.3)
+
+			# Format string to be printed to GUI
+            psi_readout_string = "{:.1f} psi".format( psi_readout )
+
 		    # Update sensor gauge readings
-            gauge1.setText("Nan", "Fuel Tank Pressure"     )
-            gauge2.setText("Nan", "Fuel Flow Rate"         )
-            gauge3.setText("Nan", "Fuel Injection Pressure")
-            gauge4.setText("Nan", "Thrust"                 )
-            gauge5.setText("Nan", "LOX Pressure"           )
-            gauge6.setText("Nan", "LOX Flow Rate"          )
-            gauge7.setText("Nan", "Engine Pressure"        )
-            gauge8.setText("Nan", "LOX Temperature"        )
+            gauge1.setText( str(psi_readout_string), "Fuel Tank Pressure"     )
+            gauge2.setText(str(psi_readout_string), "Fuel Flow Rate"         )
+            gauge3.setText(str(psi_readout_string), "Fuel Injection Pressure")
+            gauge4.setText(str(psi_readout_string), "Thrust"                 )
+            gauge5.setText(str(psi_readout_string), "LOX Pressure"           )
+            gauge6.setText(str(psi_readout_string), "LOX Flow Rate"          )
+            gauge7.setText(str(psi_readout_string), "Engine Pressure"        )
+            gauge8.setText(str(psi_readout_string), "LOX Temperature"        )
+
+            gauge1.setAngle( psi_readout )
+            gauge2.setAngle( psi_readout )
+            gauge3.setAngle( psi_readout )
+            gauge4.setAngle( psi_readout )
+            gauge5.setAngle( psi_readout )
+            gauge6.setAngle( psi_readout )
+            gauge7.setAngle( psi_readout )
+            gauge8.setAngle( psi_readout )
 
 		    # Update engine schematic
             plumbing.updatePipeStatus()
